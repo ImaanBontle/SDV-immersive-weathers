0633520677ee47866a23525d56b95560167a0ac8
ImaanBontle <80769699+ImaanBontle@users.noreply.github.com>
2022-12-02 14:02:09 +0000

    General clean up of code and enhancing of readability.

---
+++ WeatherMan.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks.Dataflow;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace ImmersiveWeathers
+{
+    // Handle weather predictions
+    internal class WeatherMan
+    {
+        // Prepare weather statements for SMAPI terminal
+        public static string Predict(IWAPI.WeatherState weatherForecast)
+        {
+            // Today's weather
+            string weatherStringToday = TodayWeather(weatherForecast);
+
+            // Tomorrow's weather
+            string weatherStringTomorrow = TomorrowWeather(weatherForecast);
+
+            string weatherString = $"{weatherStringToday} {weatherStringTomorrow}";
+            return weatherString;
+        }
+
+        // Translate today's weather state into a string.
+        private static string TodayWeather(IWAPI.WeatherState weatherForecast)
+        {
+            string weatherStringToday = "";
+            switch (weatherForecast.WeatherToday)
+            {
+                case IWAPI.WeatherType.Sunny:
+                    weatherStringToday = "It is sunny today.";
+                    break;
+                case IWAPI.WeatherType.Windy:
+                    weatherStringToday = "It is windy today.";
+                    break;
+                case IWAPI.WeatherType.Raining:
+                    weatherStringToday = "It is raining today.";
+                    break;
+                case IWAPI.WeatherType.Storming:
+                    weatherStringToday = "It is stormy today.";
+                    break;
+                case IWAPI.WeatherType.Snowing:
+                    weatherStringToday = "It is snowing today.";
+                    break;
+            }
+            return weatherStringToday;
+        }
+
+        // Translate tomorrow's weather state into a string.
+        private static string TomorrowWeather(IWAPI.WeatherState weatherForecast)
+        {
+            string weatherStringTomorrow = "";
+            switch (weatherForecast.WeatherTomorrow)
+            {
+                case IWAPI.WeatherType.Sunny:
+                    weatherStringTomorrow = "Tomorrow, it will be sunny.";
+                    break;
+                case IWAPI.WeatherType.Windy:
+                    weatherStringTomorrow = "Tomorrow, it will be windy.";
+                    break;
+                case IWAPI.WeatherType.Raining:
+                    weatherStringTomorrow = "Tomorrow, it will be rainy.";
+                    break;
+                case IWAPI.WeatherType.Storming:
+                    weatherStringTomorrow = "Tomorrow, it will be storming.";
+                    break;
+                case IWAPI.WeatherType.Snowing:
+                    weatherStringTomorrow = "Tomorrow, it will be snowing.";
+                    break;
+                case IWAPI.WeatherType.Unknown:
+                    weatherStringTomorrow = "Huh, that's weird. I can't make sense of tomorrow's weather!";
+                    break;
+            }
+            return weatherStringTomorrow;
+        }
+    }
+}
---
+++ WeatherUtils.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks.Dataflow;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace ImmersiveWeathers
+{
+    public class WeatherUtils
+    {
+        // Populate a weatherState object with current weather information
+        public class PopulateWeather
+        {
+            // Generate properties
+            public static IWAPI.WeatherState Populate(IWAPI.WeatherState weatherState)
+            {
+                // Check today's weather
+                Dictionary<string, bool> weatherStatesToday = CheckWeather();
+                weatherState.WeatherToday = ParseWeather(weatherStatesToday);
+
+                // Check tomorrow's weather
+                int tomorrowState = Game1.weatherForTomorrow;
+                weatherState.WeatherTomorrow = ParseWeather(tomorrowState);
+
+                return weatherState;
+            }
+
+            // Check today's weather states
+            private static Dictionary<string, bool> CheckWeather()
+            {
+                Dictionary<string, bool> weatherStatesToday = new(){
+                { "rainCheck", Game1.isRaining },
+                { "stormCheck", Game1.isLightning },
+                { "windCheck", Game1.isDebrisWeather },
+                { "snowCheck", Game1.isSnowing },
+            };
+                return weatherStatesToday;
+            }
+
+            // Parse today's weather states
+            private static IWAPI.WeatherType ParseWeather(Dictionary<string, bool> weatherStatesToday)
+            {
+                IWAPI.WeatherType weatherNow;
+                if (weatherStatesToday["stormCheck"] == true)
+                    weatherNow = IWAPI.WeatherType.Storming;
+                else if (weatherStatesToday["rainCheck"] == true)
+                    weatherNow = IWAPI.WeatherType.Raining;
+                else if (weatherStatesToday["snowCheck"] == true)
+                    weatherNow = IWAPI.WeatherType.Snowing;
+                else if (weatherStatesToday["windCheck"] == true)
+                    weatherNow = IWAPI.WeatherType.Windy;
+                else
+                    weatherNow = IWAPI.WeatherType.Sunny;
+                return weatherNow;
+            }
+
+            // Parse tomorrow's weather
+            private static IWAPI.WeatherType ParseWeather(int tomorrowState)
+            {
+                IWAPI.WeatherType WeatherTomorrow;
+                if ((tomorrowState == 0) || (tomorrowState == 4) || (tomorrowState == 6))
+                    WeatherTomorrow = IWAPI.WeatherType.Sunny;
+                else if (tomorrowState == 1)
+                    WeatherTomorrow = IWAPI.WeatherType.Raining;
+                else if (tomorrowState == 2)
+                    WeatherTomorrow = IWAPI.WeatherType.Windy;
+                else if (tomorrowState == 3)
+                    WeatherTomorrow = IWAPI.WeatherType.Storming;
+                else if (tomorrowState == 5)
+                    WeatherTomorrow = IWAPI.WeatherType.Snowing;
+                else
+                    WeatherTomorrow = IWAPI.WeatherType.Unknown;
+                return WeatherTomorrow;
+            }
+        }
+
+        public class TranslateWeatherStates
+        {
+            public static string TranslateTomorrow(int integerState)
+            {
+                string stringState = "";
+                foreach (int i in Enum.GetValues(typeof(IWAPI.WeatherType)))
+                {
+                    if (integerState == i)
+                    {
+                        stringState = Enum.GetName(typeof(IWAPI.WeatherType), i);
+                        break;
+                    }
+                }
+                return stringState;
+            }
+
+            public static int TranslateTomorrow(string stringState)
+            {
+                int integerState = 7;
+                foreach (string name in Enum.GetNames(typeof(IWAPI.WeatherType)))
+                {
+                    if (stringState == name)
+                    {
+                        integerState = (int)Enum.Parse(typeof(IWAPI.WeatherType), name);
+                        break;
+                    }
+                }
+                return integerState;
+            }
+        }
+    }
+}
