ca3a1da88e9756039287d5e2b9c3e928cb92543a
ImaanBontle <80769699+ImaanBontle@users.noreply.github.com>
2022-12-13 18:16:08 +0000

    Generic Mod Config Menu now supported (#27)
    * feat: added GMCM API
    
    GMCM API integrated into mod.
    
    * feat: implemented current config options into GMCM
    
    This only includes the two logging methods. No other config options yet exist.
    
    * fix: updated changelog

---
+++ GMCMHandler.cs
@@ -0,0 +1,47 @@
+﻿using GenericModConfigMenu;
+using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImmersiveWeathers
+{
+    internal class GMCMHandler
+    {
+        internal static void Register(ModConfig Config, IGenericModConfigMenuApi gMCM, StardewModdingAPI.IManifest ModManifest, IModHelper Helper)
+        {
+            // Register mod
+            gMCM.Register(
+                mod: ModManifest,
+                reset: () => Config = new ModConfig(),
+                save: () => Helper.WriteConfig(Config)
+                );
+
+            // Add section title
+            gMCM.AddSectionTitle(
+                mod: ModManifest,
+                text: () => "Weather Reports",
+                tooltip: () => "Each morning, players can choose to receive a forecast of the weather today and tomorrow. These options determine whether the player receives these messages, and if so, how."
+                );
+
+            // Add terminal logging
+            gMCM.AddBoolOption(
+                mod: ModManifest,
+                getValue: () => Config.PrintToTerminal,
+                setValue: value => Config.PrintToTerminal = value,
+                name: () => "SMAPI Terminal",
+                tooltip: () => "If true, weather predictions are printed to the SMAPI terminal."
+                );
+
+            // Add HUD logging
+            gMCM.AddBoolOption(
+                mod: ModManifest,
+                getValue: () => Config.PrintHUDMessage,
+                setValue: value => Config.PrintHUDMessage = value,
+                name: () => "In-Game HUD",
+                tooltip: () => "If true, weather predictions are printed using the in-game HUD.");
+        }
+    }
+}
